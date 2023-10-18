69d089836bfdb7f6fb42c5304fa4ab4ba2db2bf7
unknown <GVIT 4@DESKTOP-BKV46MQ.gvit.server>
2023-10-18 16:36:27 +0530

    second commit

--- gvit_first_do_project/settings.py
+++ gvit_first_do_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%xex^vojcjzotuv08aff!i&7wx!0uc!8ya#5hsda1xw_%)(a1_'
+# SECRET_KEY = 'django-insecure-%xex^vojcjzotuv08aff!i&7wx!0uc!8ya#5hsda1xw_%)(a1_'
+
+SECRET_KEY = os.getenv('SECRET_KEY' , 'django-insecure-%xex^vojcjzotuv08aff!i&7wx!0uc!8ya#5hsda1xw_%)(a1_')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+DEBUG = os.getenv('DEBUG' , 'True') == 'True'
 
 ALLOWED_HOSTS = []
 
