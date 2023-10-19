497f4ec6c89380c8e127310f588f9e297dae4b6e
unknown <GVIT 4@DESKTOP-BKV46MQ.gvit.server>
2023-10-19 13:25:22 +0530

    static files commit

--- gvit_app/views.py
+++ gvit_app/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def Index(request):
     return render(request , "index.html")
+
+
+def another_page(request):
+    return render(request , "another_page.html")
